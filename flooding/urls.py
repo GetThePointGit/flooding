158b445171d91c71b637fd329fc3b8c58be4c192
reinout <reinout@vanrees.org>
2010-04-22 09:54:59 +0000

    Basic url setup

--- flooding/urls.py
+++ flooding/urls.py
@@ -2,12 +2,16 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
 
+import lizard_base.urls
+import lizard_flooding.urls
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     (r'^admin/', include(admin.site.urls)),
+    (r'^flooding/', include(lizard_flooding.urls)),
+    (r'', include(lizard_base.urls)),
     )
 
 
