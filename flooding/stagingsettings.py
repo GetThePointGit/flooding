c11e59711b2e7a2e6457767bf358a48fbea45f82
Remco Gerlich <remco@gerlich.nl>
2012-03-01 08:39:19 +0000

    Fixes to make fabfile work

---
+++ flooding/stagingsettings.py
@@ -0,0 +1,13 @@
+from flooding.settings import *
+
+DATABASES = {
+    # Changed server from production to staging
+    'default': {
+        'NAME': 'floodingtest',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'USER': 'floodingtest',
+        'PASSWORD': 'feequ2Re',
+        'HOST': 'p-flod-db-00-d1.external-nens.local',
+        'PORT': '',
+        },
+    }
