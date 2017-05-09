4e8a6cdfdab3e76017e263b5545439ad6a212aa4
AlexandrSeleznev <a.seleznev@hotmail.nl>
2017-05-09 09:08:29 -0700

    Add GD-tool.

--- flooding/settings.py
+++ flooding/settings.py
@@ -176,6 +176,7 @@ INSTALLED_APPS = (
                      # approvaltool
     'flooding_lib.tools.importtool',
     'flooding_lib.tools.exporttool',
+    'flooding_lib.tools.gdmapstool',
     'django.contrib.staticfiles',
     'raven.contrib.django.raven_compat',
     'south',
